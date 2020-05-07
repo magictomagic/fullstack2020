440cb9de7eba0e6dc8d9431bb732ec3f55037bca
magictomagic <magictomagic@z1172765646@gmail.com>
2020-05-07 23:19:03 +0800

    new file:   exp8/affine_transform.m 	new file:   exp8/demo_harris_affine.m 	new file:   exp8/detHarrisCorners.m 	new file:   exp8/est_optimal_Affine_ransac.m 	new file:   exp8/est_optimal_affine.m 	new file:   exp8/extractNccFeature.m 	new file:   exp8/harris2img_ransac.png 	new file:   exp8/harris2img_raw.png 	new file:   exp8/img/1/harris2img_raw.png 	new file:   exp8/img/1/pic3.png 	new file:   exp8/img/1/pic3_gray.png 	new file:   exp8/img/1/pic4.png 	new file:   exp8/img/1/pic4_gray.png 	new file:   exp8/img/2/harris2img_raw.png 	new file:   exp8/img/3/harris2img_raw.png 	new file:   exp8/img/3/pic3.png 	new file:   exp8/img/3/pic3_gray.png 	new file:   exp8/img/3/pic4.png 	new file:   exp8/img/3/pic4_gray.png 	new file:   exp8/img/4/harris2img_raw.png 	new file:   exp8/img/4/pic3.png 	new file:   exp8/img/4/pic3_gray.png 	new file:   exp8/img/4/pic4.png 	new file:   exp8/img/4/pic4_gray.png 	new file:   exp8/img/IMG_20200428_235357.png 	new file:   exp8/img/IMG_20200428_235400.png 	new file:   exp8/img/IMG_20200429_012218.jpg 	new file:   exp8/img/IMG_20200429_012221.jpg 	new file:   exp8/pic3.jpg 	new file:   exp8/pic3_gray.png 	new file:   exp8/pic4.jpg 	new file:   exp8/pic4_gray.png

---
+++ exp8/extractNccFeature.m
@@ -0,0 +1,40 @@
+function descps = extractNccFeature(img, Locs, halfsz)
+
+% parse the input parameters
+if(~exist('halfsz','var'))
+   halfsz = [12,12];
+else
+    if(length(halfsz) <= 1)
+        halfsz = [halfsz, halfsz];
+    else
+        halfsz = halfsz(1:2);
+    end
+end
+halfsz = round(halfsz);
+halfsz(halfsz<1) = 1;
+
+%% ï¿½ï¿½È¡ï¿½ï¿½ï¿½ï¿½ï¿?
+
+nc = size(img,3);
+dim = prod(2*halfsz+1);
+descps = zeros(size(Locs,1), nc * dim);  
+
+img = double(img);
+
+for i = 1 : size(Locs,1)
+    x = Locs(i,1);
+    y = Locs(i,2);
+    
+    xlo = max([1, x - halfsz(1)]);
+    xhi = min([size(img,2), x + halfsz(1)]);
+    ylo = max([1, y - halfsz(2)]);
+    yhi = min([size(img,1), y + halfsz(2)]);
+ % add   
+    count = (xhi - xlo + 1) * (yhi - ylo + 1);
+    data = img(ylo:yhi, xlo:xhi);
+    descps(i, 1:count) = data(:);
+end
+
+% do the normalization
+descps = descps - repmat(mean(descps,2),[1 nc * dim]);
+descps = descps ./ repmat(sqrt(sum(descps.^2,2)+1e-20),[1 nc*dim]);
